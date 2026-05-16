720d1e1fd008e4004e6ecdc9dc3fdad4a4f44d01
Medsoftwz <xyz22@proximus.be>
2026-05-16 16:48:21 +0200

    Add files via upload

---
+++ neuro.js
@@ -0,0 +1,186 @@
+const WZtree = [
+ 
+ { code: "U", terms: ["Neurologisch onderzoek (procedure)"] },
+
+  { code: "UA", terms: ["Bewustzijnsonderzoek (procedure)"] },
+  { code: "UA1", terms: ["Graad van bewustzijn (observable)"] },
+  { code: "UA2", terms: ["Glasgow Coma Scale beoordeling (observable)"] },
+
+  { code: "UB", terms: ["Motorisch onderzoek (procedure)"] },
+  { code: "UB1", terms: ["Spierkracht beoordeling (observable)"] },
+  { code: "UB2", terms: ["Spiertonus beoordeling (observable)"] },
+  { code: "UB3", terms: ["Onderzoek naar nekstijfheid (procedure)"] },
+  { code: "UB4", terms: ["Tremor beoordeling (observable)"] },
+
+  { code: "UC", terms: ["Sensibiliteitsonderzoek (procedure)"] },
+  { code: "UC1", terms: ["Tastzin beoordeling (observable)"] },
+  { code: "UC2", terms: ["Pijnzin beoordeling (observable)"] },
+  { code: "UC3", terms: ["Temperatuurzin beoordeling (observable)"] },
+  { code: "UC4", terms: ["Diepe sensibiliteit beoordeling (observable)"] },
+  { code: "UC5", terms: ["Stemvorktest (procedure)"] },
+
+  { code: "UD", terms: ["Reflexonderzoek (procedure)"] },
+  { code: "UD1", terms: ["Peesreflex beoordeling (observable)"] },
+  { code: "UD2", terms: ["Voetzoolreflex beoordeling (procedure)"] },
+  { code: "UD3", terms: ["Corneareflex beoordeling (procedure)"] },
+
+  { code: "UE", terms: ["Stamreflexonderzoek (procedure)"] },
+  { code: "UE1", terms: ["Corneomandibulaire reflex beoordeling (procedure)"] },
+  { code: "UE2", terms: ["Palmomentale reflex beoordeling (procedure)"] },
+  { code: "UE3", terms: ["Snautreflex beoordeling (procedure)"] },
+
+  { code: "UF", terms: ["Evenwichtsonderzoek (procedure)"] },
+  { code: "UF1", terms: ["Romberg test (procedure)"] },
+  { code: "UF2", terms: ["Wijsvingerproef (procedure)"] },
+  { code: "UF3", terms: ["Teken van Weill (procedure)"] },
+
+  { code: "UG", terms: ["Coördinatieonderzoek (procedure)"] },
+  { code: "UG1", terms: ["Vinger-neus test (procedure)"] },
+  { code: "UG2", terms: ["Adiadochokinesetest (procedure)"] },
+
+  { code: "UH", terms: ["Craniale zenuwonderzoek (procedure)"] },
+  { code: "UH1", terms: ["Oculomotorische zenuwonderzoek (procedure)"] },
+  { code: "UH2", terms: ["Trigeminusonderzoek (procedure)"] },
+  { code: "UH3", terms: ["Facialisonderzoek (procedure)"] },
+  { code: "UH4", terms: ["Glossopharyngeus/Vagus onderzoek (procedure)"] },
+  { code: "UH5", terms: ["Vestibulocochlearis onderzoek (procedure)"] },
+
+  { code: "UK", terms: ["Sfincteronderzoek (procedure)"] },
+    { code: "UK1", terms: ["Anale sfincter onderzoek (procedure)"] },
+	{ code: "UK2", terms: ["Uretrale sfincter onderzoek (procedure)"] },
+	{ code: "UK3", terms: ["Pupil sfincter onderzoek (procedure)"] },
+	{ code: "UK4", terms: ["Slik sfincter onderzoek (procedure)"] },
+  
+  { code: "UL", terms: ["Onderzoek van autonoom zenuwstelsel (procedure)"] },
+    { code: "UL2", terms: ["Interoceptie"] },
+
+  { code: "UM", terms: ["Zintuigonderzoek (procedure)"] },
+    { code: "UM1", terms: ["Oogonderzoek (procedure)"] },
+    { code: "UM1A", terms: ["Visus beoordeling (observable)"] },
+	{ code: "UM1B", terms: ["Oogbewegingsonderzoek (procedure)"] },
+	{ code: "UM1C", terms: ["Nystagmus beoordeling (observable)"] },
+	{ code: "UM1D", terms: ["Gezichtsveldonderzoek (procedure)"] },
+	{ code: "UM1E", terms: ["Pupilreactie beoordeling (observable)"] },
+	{ code: "UM1F", terms: ["Corneagevoeligheid beoordeling (observable)"] },
+    { code: "UM2", terms: ["Gehooronderzoek (procedure)"] },
+	{ code: "UM2A", terms: ["Geluid - Toonhoogtes"] },
+	{ code: "UM2B", terms: ["Richting van het geluid"] },
+  { code: "UM5", terms: ["Reukonderzoek (procedure)"] },
+	{ code: "UM5A", terms: ["Geur"] },
+	{ code: "UM5B", terms: ["Chemische stoffen"] },
+  { code: "UM3", terms: ["Smaakonderzoek (procedure)"] },
+  	{ code: "UM3A", terms: ["Zoet"] },
+	{ code: "UM3B", terms: ["Zuur"] },
+	{ code: "UM3C", terms: ["Bitter"] },
+	{ code: "UM3D", terms: ["Umami"] },
+  { code: "UM4", terms: ["Huid: tastgevoel - somatosensorisch (procedure)"] },
+	{ code: "UM4A", terms: ["Druk"] },
+	{ code: "UM4B", terms: ["Temperatuur"] },
+	{ code: "UM4C", terms: ["Pijn"] },
+	{ code: "UM4D", terms: ["Trillingen"] },
+ { code: "UM6", terms: ["Proprioceptie"] },
+ 
+   { "code": "UP",   "terms": ["Neurologisch onderzoek - andere versie"] },
+
+  { "code": "UP1",  "terms": ["Autonoom zenuwstelsel - Sfincters"] },
+
+  { "code": "UP2",  "terms": ["Bewustzijn: comaschaal"] },
+
+  { "code": "UP3",  "terms": ["Craniale zenuwen"] },
+  { "code": "UP3A", "terms": ["Craniale zenuwen: 3-4-6"] },
+  { "code": "UP3B", "terms": ["Craniale zenuwen: 5"] },
+  { "code": "UP3C", "terms": ["Craniale zenuwen: 7"] },
+  { "code": "UP3D", "terms": ["Craniale zenuwen: 8-1-2"] },
+  { "code": "UP3E", "terms": ["Craniale zenuwen: 9-10-11"] },
+
+  { "code": "UP4",  "terms": ["Evenwicht & Coördinatie"] },
+
+  { "code": "UP4A", "terms": ["Evenwicht"] },
+  { "code": "UP4A1", "terms": ["Romberg proef"] },
+  { "code": "UP4A2", "terms": ["Weill – teken van"] },
+
+  { "code": "UP4B", "terms": ["Coördinatie"] },
+  { "code": "UP4B1", "terms": ["Adiadochokinesis"] },
+  { "code": "UP4B2", "terms": ["Vinger-neus proef"] },
+
+  { "code": "UP5",  "terms": ["Motoriek"] },
+  { "code": "UP5A", "terms": ["Nekstijfheid"] },
+  { "code": "UP5B", "terms": ["Spierkracht"] },
+  { "code": "UP5C", "terms": ["Tonus"] },
+  { "code": "UP5D", "terms": ["Tremor"] },
+
+  { "code": "UP6",  "terms": ["Reflexen"] },
+  { "code": "UP6A", "terms": ["Oppervlakkige reflexen"] },
+  { "code": "UP6A1", "terms": ["Peesreflexen"] },
+  { "code": "UP6A2", "terms": ["Voetzoolreflexen"] },
+  { "code": "UP6A3", "terms": ["Corneareflex"] },
+
+  { "code": "UP6B", "terms": ["Stamreflexen"] },
+  { "code": "UP6B1", "terms": ["Corneomandibulair reflex"] },
+  { "code": "UP6B2", "terms": ["Palmomentaal reflex"] },
+  { "code": "UP6B3", "terms": ["Snautreflex"] },
+
+  { "code": "UP7",  "terms": ["Sensibiliteit"] },
+  { "code": "UP7A", "terms": ["Tastzin"] },
+  { "code": "UP7B", "terms": ["Pijn- en temperatuurgevoel"] },
+  { "code": "UP7C", "terms": ["Diepe gevoeligheid"] },
+  { "code": "UP7D", "terms": ["Stemvorkproef"] },
+
+  { "code": "UP8",  "terms": ["Zintuigen"] },
+
+  { "code": "UP8A", "terms": ["Ogen"] },
+  { "code": "UP8A1", "terms": ["Pupil"] },
+  { "code": "UP8A2", "terms": ["Cornea"] },
+  { "code": "UP8A3", "terms": ["Oogbewegingen"] },
+  { "code": "UP8A4", "terms": ["Nystagmus"] },
+  { "code": "UP8A5", "terms": ["Gezichtsveld"] },
+  { "code": "UP8A6", "terms": ["Visus (het zien)"] },
+
+  { "code": "UP8B", "terms": ["Oren: het gehoor"] },
+
+  { "code": "UP8C", "terms": ["Mond: smaak en reuk"] },
+
+  { "code": "UP8D", "terms": ["Handen: tastgevoel"] },
+
+  { "code": "UP8E", "terms": ["Neus: het ruiken"] },
+
+  { "code": "UQ",  "terms": ["Neurologische uitval"] },
+
+  { "code": "UQ1",   "terms": ["Motorische uitval"] },
+  { "code": "UQ1A",  "terms": ["Een arm of been dat plots zwakker wordt"] },
+  { "code": "UQ1B",  "terms": ["Scheef hangen van het gezicht"] },
+  { "code": "UQ1C",  "terms": ["Moeite met grijpen, stappen of opstaan"] },
+
+  { "code": "UQ2",   "terms": ["Sensibele uitval"] },
+  { "code": "UQ2A",  "terms": ["Tintelingen"] },
+  { "code": "UQ2B",  "terms": ["Doof gevoel of verminderd gevoel"] },
+  { "code": "UQ2C",  "terms": ["Verminderd pijn- of temperatuurgevoel"] },
+
+  { "code": "UQ3",   "terms": ["Visuele uitval"] },
+  { "code": "UQ3A",  "terms": ["Wazig zien"] },
+  { "code": "UQ3B",  "terms": ["Dubbelzien"] },
+  { "code": "UQ3C",  "terms": ["Gezichtsveldverlies (hemianopsie)"] },
+  { "code": "UQ3D",  "terms": ["Plots verlies van zicht in één oog (amaurosis fugax)"] },
+
+  { "code": "UQ4",   "terms": ["Spraak- en taalstoornissen"] },
+  { "code": "UQ4A",  "terms": ["Moeite om woorden te vinden"] },
+  { "code": "UQ4B",  "terms": ["Onbegrijpelijke of verwarde spraak"] },
+  { "code": "UQ4C",  "terms": ["Niet begrijpen wat anderen zeggen"] },
+
+  { "code": "UQ5",   "terms": ["Coördinatiestoornissen (ataxie)"] },
+  { "code": "UQ5A",  "terms": ["Onzekere, wankele gang"] },
+  { "code": "UQ5B",  "terms": ["Misgrijpen (dysmetrie)"] },
+  { "code": "UQ5C",  "terms": ["Niet recht kunnen blijven staan"] },
+
+  { "code": "UQ6",   "terms": ["Bewustzijns- of cognitieve stoornissen"] },
+  { "code": "UQ6A",  "terms": ["Verwardheid"] },
+  { "code": "UQ6B",  "terms": ["Verminderde alertheid"] },
+  { "code": "UQ6C",  "terms": ["Plots geheugenverlies"] },
+
+  { "code": "UQ7",   "terms": ["Craniale zenuwuitval"] },
+  { "code": "UQ7A",  "terms": ["Scheef gezicht (n. facialis)"] },
+  { "code": "UQ7B",  "terms": ["Dubbelzien door oogbewegingsstoornis"] },
+  { "code": "UQ7C",  "terms": ["Slikproblemen"] },
+  { "code": "UQ7D",  "terms": ["Heesheid of stemverandering"] }
+
+];
